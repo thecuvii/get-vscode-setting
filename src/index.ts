026c0eef02fa76e8181073030aa8091dbf72e960
withyellow <xw5427@gmail.com>
2023-02-05 11:48:49 +0800

    feat: implement core feature

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,48 @@
-export const one = 1;
-export const two = 2;
+/**
+ *
+ * Windows %APPDATA%\Code\User\settings.json
+ * macOS $HOME/Library/Application\ Support/Code/User/settings.json
+ * Linux $HOME/.config/Code/User/settings.json
+ * @see https://code.visualstudio.com/docs/getstarted/settings#_settings-file-locations
+ */
+
+import { existsSync, readFileSync } from 'fs';
+import { join } from 'pathe';
+
+const getSetting = (homekey: string, path: string) => {
+  let fullpath = '';
+  let settings = '{}';
+  const home = process.env[homekey];
+  if (!home) throw new Error(`unknown homekey`);
+  fullpath = join(home, path);
+  if (existsSync(fullpath)) {
+    settings = readFileSync(fullpath, 'utf-8');
+  }
+
+  return {
+    path: fullpath,
+    settings,
+  };
+};
+
+export const getVSCodeSettings = () => {
+  let settings = '{}';
+  let path = '';
+
+  switch (process.platform) {
+    case 'darwin': {
+      return getSetting('HOME', 'Library/Application Support/Code/User/settings.json');
+    }
+    case 'win32': {
+      return getSetting('APPDATA', `Code/User/settings.json`);
+    }
+    case 'linux': {
+      return getSetting('HOME', '.config/Code/User/settings.json');
+    }
+  }
+
+  return {
+    path,
+    settings,
+  };
+};
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,53 @@
-import { describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { getVSCodeSettings } from '../src';
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
-  })
-})
+const mockPlatform = (platform: string) => {
+  const originalPlatform = process.platform;
+  Object.defineProperty(process, 'platform', { value: platform });
+
+  return () => {
+    Object.defineProperty(process, 'platform', { value: originalPlatform });
+  };
+};
+
+describe('getVSCodeSettings', () => {
+  afterEach(() => {
+    vi.unstubAllEnvs();
+  });
+
+  it('macos', () => {
+    const restorePlatform = mockPlatform('darwin');
+
+    vi.stubEnv('HOME', 'withyellow');
+
+    const ret = getVSCodeSettings();
+
+    expect(ret.path).toBe('withyellow/Library/Application Support/Code/User/settings.json');
+
+    restorePlatform();
+  });
+
+  it('windows', () => {
+    const restorePlatform = mockPlatform('win32');
+
+    vi.stubEnv('APPDATA', 'withyellow');
+
+    const ret = getVSCodeSettings();
+
+    expect(ret.path).toBe(`withyellow/Code/User/settings.json`);
+
+    restorePlatform();
+  });
+
+  it('linux', () => {
+    const restorePlatform = mockPlatform('linux');
+
+    vi.stubEnv('HOME', 'withyellow');
+
+    const ret = getVSCodeSettings();
+
+    expect(ret.path).toBe(`withyellow/.config/Code/User/settings.json`);
+
+    restorePlatform();
+  });
+});
